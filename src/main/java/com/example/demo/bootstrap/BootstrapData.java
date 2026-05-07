1b3747e68576dce7a4510c40c32c236855ba1ce4
Lim Sze Ming Stephanie <64758733+K-156@users.noreply.github.com>
2026-05-08 07:53:24 +0800

    Added new Publisher entity

--- src/main/java/com/example/demo/bootstrap/BootstrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package com.example.demo.bootstrap;
 
 import com.example.demo.domain.Author;
 import com.example.demo.domain.Book;
+import com.example.demo.domain.Publisher;
 import com.example.demo.repository.AuthorRepository;
 import com.example.demo.repository.BookRepository;
+import com.example.demo.repository.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -29,8 +33,16 @@ public class BootstrapData implements CommandLineRunner {
         b1.setTitle("Head First Design Patterns");
         b1.setIsbn("123");
 
+        Publisher p1 = new Publisher();
+        p1.setPublisherName("Candid Publishing");
+        p1.setAddress("8 Jalan Bukit Merah #08-21");
+        p1.setCity("Singapore");
+        p1.setState("Singapore");
+        p1.setZip("S765340");
+
         Author andrewSaved = authorRepository.save(andrew);
         Book b1Saved = bookRepository.save(b1);
+        Publisher p1Saved = publisherRepository.save(p1);
 
         /* Need to add the data to the owning side or else nothing gets written to the junction table */
         b1Saved.getAuthors().add(andrewSaved);
@@ -39,6 +51,7 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
 
 
     }
